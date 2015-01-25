8dde0e9853175f2856f234201c32181d13a52d25
Adrian Huna <huna.adrian@gmail.com>
2015-01-25 23:47:54 +0100

    Add bootstrap switch

--- js/popup.js
+++ js/popup.js
@@ -2,5 +2,8 @@ $(function() {
     $("#options").click(function() {
         chrome.tabs.create({url: "/html/options.html"});
     });
-
+    $("#switch").bootstrapSwitch();
+    $("#switch").on('switchChange.bootstrapSwitch', function(event, state) {
+        $("#info").css("visibility") == "hidden" ? $("#info").css("visibility", "visible") : $("#info").css("visibility", "hidden");
+    });
 });

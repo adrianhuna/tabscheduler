34aaa20ccfc68f9eef9352e0cd247b1906a27329
Adrian Huna <huna.adrian@gmail.com>
2015-01-29 11:18:27 +0100

    Update to jQuery

--- js/options.js
+++ js/options.js
@@ -1,10 +1,4 @@
-function save_options() {
-
-}
-
-function restore_options() {
-
-}
-
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+function restore_options() {}
+$(function() {
+    restore_options();
+});

af3107ed69c93303acd4c46a36fcc80310e8d3b3
Adrian Huna <huna.adrian@gmail.com>
2015-01-30 22:10:25 +0100

    Add option to save sites list

--- js/options.js
+++ js/options.js
@@ -1,20 +1,27 @@
+function addToListHtml(url) {
+    $("#list").append('<li><a href="' + url + '" target="_blank">' + url + '</a> - <button class="btn btn-danger btn-xs delete-site">Remove</button></li>');
+}
+
 function restoreOptions() {
-    chrome.storage.local.get(["time", "enabled"], function(r) {
+    chrome.storage.local.get(["time", "enabled", "sites"], function(r) {
         $("#time").val(r.time);
         if (r.enabled && !$("#switch").bootstrapSwitch('state')) {
-            $("#switch").bootstrapSwitch('state', true);
+            $("#switch").bootstrapSwitch('state', true, true);
         }
-        console.log($("#switch").state);
-        console.log(r.enabled === true);
+        if (r.sites.length == 0) {
+            $("#list").append('Empty');
+        }
+        for (var i = 0; i < r.sites.length; i++) {
+            addToListHtml(r.sites[i]);
+        }
     });
 }
 
 function enable() {
-    console.log(Date.now());
     chrome.alarms.create("scheduler", {
         when: Date.now() + 30000,
         periodInMinutes: 1440
-    })
+    });
     chrome.storage.local.set({
         "enabled": true
     });
@@ -43,6 +50,36 @@ function updateTime() {
         message("success", "Settings saved");
     });
 }
+
+function addToList(url) {
+    if (url.substring(0, 7) != "http://") {
+        url = "http://" + url;
+    }
+    addToListHtml(url);
+    chrome.storage.local.get("sites", function(r) {
+        var array = r.sites;
+        array.push(url);
+        chrome.storage.local.set({
+            "sites": array
+        });
+    });
+}
+
+function removeFromList(t) {
+    chrome.storage.local.get("sites", function(r) {
+        var array = r.sites;
+        var index = array.indexOf(t.prev("a").attr("href"));
+        if (index > -1) {
+            array.splice(index, 1);
+            chrome.storage.local.set({
+                "sites": array
+            });
+            t.parent("li").remove();
+        } else {
+            message("danger", "Error: We couln't remove the site. Please try again.");
+        }
+    });
+}
 $(function() {
     restoreOptions();
     $("#switch").bootstrapSwitch();
@@ -52,5 +89,13 @@ $(function() {
     $("#update-time").submit(function(e) {
         e.preventDefault();
         updateTime();
+    });
+    $("#add-site").submit(function(e) {
+        e.preventDefault();
+        addToList($("#url").val().trim());
+        $("#url").val('');
+    });
+    $(document).on("click", ".delete-site", function(e) {
+        removeFromList($(this));
     });
 });

514ba82795cb19181c0eeb96951384c55bce2354
Adrian Huna <huna.adrian@gmail.com>
2015-01-31 12:20:32 +0100

    Add functionality to popup window

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,28 @@
+function updateView() {
+    chrome.storage.local.get(["time", "enabled", "sites"], function(r) {
+        $("#time").text(r.time);
+        if (r.enabled) {
+            $("#switch").bootstrapSwitch('state', true, true);
+            $("#info").css("visibility", "visible");
+        }
+        $("#sites-count").text(r.sites.length);
+    });
+}
 $(function() {
+    updateView();
     $("#options").click(function() {
-        chrome.tabs.create({url: "/html/options.html"});
+        chrome.tabs.create({
+            url: "/html/options.html"
+        });
     });
     $("#switch").bootstrapSwitch();
     $("#switch").on('switchChange.bootstrapSwitch', function(event, state) {
-        $("#info").css("visibility") == "hidden" ? $("#info").css("visibility", "visible") : $("#info").css("visibility", "hidden");
+        if (state) {
+            $("#info").css("visibility", "visible");
+            enable();
+        } else {
+            $("#info").css("visibility", "hidden");
+            disable();
+        }
     });
 });

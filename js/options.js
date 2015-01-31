882b43e349e0f3f0f9272aad2161a94f8c1f9df6
Adrian Huna <huna.adrian@gmail.com>
2015-01-31 12:07:22 +0100

    Moved shared functionality to shared file

--- js/options.js
+++ js/options.js
@@ -17,39 +17,8 @@ function restoreOptions() {
     });
 }
 
-function enable() {
-    setAlarm();
-    chrome.storage.local.set({
-        "enabled": true
-    });
-}
-
-function disable() {
-    chrome.alarms.clearAll();
-    chrome.storage.local.set({
-        "enabled": false
-    });
-}
-
 function message(type, msg) {
     $("#alert").text(msg).removeClass().addClass("alert alert-" + type).hide().fadeIn();
-}
-
-function setAlarm() {
-    chrome.storage.local.get("time", function(r) {
-        var current = new Date();
-        var t = r.time.split(":");
-        var date = new Date(current.getFullYear(), current.getMonth(), current.getDate(), parseInt(t[0]), parseInt(t[1]));
-        if (date < current) {
-            current.setDate(current.getDate() + 1);
-            date = new Date(current.getFullYear(), current.getMonth(), current.getDate(), parseInt(t[0]), parseInt(t[1]));
-        }
-        chrome.alarms.clearAll();
-        chrome.alarms.create("scheduler", {
-            when: date.getTime(),
-            periodInMinutes: 1440
-        });
-    });
 }
 
 function updateTime() {
